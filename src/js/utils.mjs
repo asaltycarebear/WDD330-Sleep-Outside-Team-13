0f17a92b540279e28f3993347a31df3ad6f86642
asaltycarebear <garbear.stockwell@gmail.com>
2026-09-17 14:07:25 -0600

    Step3 part 9

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -38,3 +38,23 @@ export function renderListWithTemplate(template, parentElement, list, position =
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+//need to add footer here.
+export async function loadHeaderFoother() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  renderWithTemplate(headerTemplate, headerElement);
+}
