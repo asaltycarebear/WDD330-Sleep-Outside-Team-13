f3a70a556de8749dccc4196954f72b3cbb35c724
asaltycarebear <garbear.stockwell@gmail.com>
2026-09-21 01:18:53 -0600

    variable fix

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import ProductDetails from "./ProductDetails.mjs";
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
-const product = new ProductDetails(productID, dataSource);
+const product = new ProductDetails(productId, dataSource);
 product.init();
 
 function addProductToCart(product) {
