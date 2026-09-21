427af4346ece3ff09e4c819f7fc2d6196138bdc0
asaltycarebear <garbear.stockwell@gmail.com>
2026-09-21 02:52:44 -0600

    Remove from cart added

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter, setLocalStorage } from "./utils.mjs";
 
 loadHeaderFooter();
 
@@ -8,23 +8,66 @@ function renderCartContents() {
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
+// function cartItemTemplate(item) {
+//   const newItem = `<li class="cart-card divider">
+//   <a href="#" class="cart-card__image">
+//     <img
+//       src="${item.Image}"
+//       alt="${item.Name}"
+//     />
+//   </a>
+//   <a href="#">
+//     <h2 class="card__name">${item.Name}</h2>
+//   </a>
+//   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+//   <p class="cart-card__quantity">qty: 1</p>
+//   <p class="cart-card__price">$${item.FinalPrice}</p>
+// </li>`;
+
+//   return newItem;
+// }
+
+//Below added new remove from cart button
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+    <button class="remove-from-cart" data-id="${item.Id}">
+      Remove
+    </button>
+  </li>`;
 
   return newItem;
 }
+
+//Below is added function for week 2 indavidual activity, cgs
+function removeFromCart(productId) {
+  const cartItems = getLocalStorage("so-cart") || [];
+
+  const updatedCart = cartItems.filter((item) => item.Id !== productId);
+
+  setLocalStorage("so-cart", updatedCart);
+
+  renderCartContents();
+}
+
+document.querySelector(".product-list").addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-from-cart")) {
+    const productId = event.target.dataset.id;
+    removeFromCart(productId);
+  }
+});
 
 renderCartContents();
